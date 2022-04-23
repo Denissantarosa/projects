10ed0685ca106e8eae2bbfdb243ad52569f2616c
denis <denissantarosa@gmail.com>
2022-04-23 08:50:07 -0300

    correcao no somatorio

--- calculador.py
+++ calculador.py
@@ -1,5 +1,9 @@
 class Calculador:
-  
-  def somatorio(self, valor1, valor2):
-      print("Realizando soma")
-      return valo1 + valor2
+
+    def __init__(self, valor1, valor2):
+        self.__valor1 = valor1
+        self.__valor2 = valor2
+
+    def somatorio(self):
+        print("Realizando soma")
+        return __valor1 + __valor2
